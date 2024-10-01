94cbda2abcbe7f5a0244d120dcb5fbe9fbfde1b5
Annoir Idrissa Hamani <annoiridirssa@gmail.com>
2024-10-01 10:54:00 -0300

    Ajoutez des fichiers projet.

---
+++ HelloGitHubVs/HelloGitHubVsForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HelloGitHubVs
+{
+    partial class HelloGitHubVsForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(140, 103);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(150, 40);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "Hello GitHub";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.helloButton_Click);
+            // 
+            // HelloGitHubVsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 294);
+            this.Controls.Add(this.helloButton);
+            this.Name = "HelloGitHubVsForm";
+            this.Text = "GitHub et Git pour visual Studio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloButton;
+    }
+}
+
---
+++ HelloGitHubVs/HelloGitHubVsForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloGitHubVs
+{
+    public partial class HelloGitHubVsForm : Form
+    {
+        public HelloGitHubVsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello GitHub");
+        }
+    }
+}
